7ead6841b9ee8e21a9962b636d1d34422df8d2c9
Rashmi Shree S <rashmishrees@Rashmis-MacBook-Pro.local>
2022-08-29 12:10:44 +0530

    er

--- BE/delivery.js
+++ BE/delivery.js
@@ -30,7 +30,7 @@ router.post('/getDeliveryReportData', (req,res)=>{
     const invoice_no = reqdata.invoiceNo;
     const customer_reference_no = reqdata.customer_reference_no;
     db.query(
-        `select * from delivery_report_table
+        `select * from delivery_report_table, product_details_table
          where phone_number = ? or customer_name = ?
         or invoice_no = ? or customer_reference_no = ?`,
         [
