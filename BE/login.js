227663fdc0840441451e244f0e6d796d1009152c
Rashmi Shree S <rashmishrees@Rashmis-MacBook-Pro.local>
2022-09-12 16:04:57 +0530

    er

--- BE/login.js
+++ BE/login.js
@@ -13,43 +13,43 @@ router.post('/login',(req,res)=>{
     const reqestData = req.body.params.logindata;
     const username = reqestData.username;
     const password = reqestData.password;
-    console.log("hiiii. username", username);
-    if (username == undefined){
-        console.log("nothing");
-    }
-    else {
-        console.log("something");
-    }
-    // db.query(`select username from users where username = ?`,
-    // [username],
-    // (err,userresponse)=>{
-    //     if (userresponse.length>0){
-    //         db.query(
-    //             `select username, password
-    //              from users where username = ? and password = ?`,
-    //             [username, password],
-    //             (err, result)=>{
-    //                 if (err){
-    //                     console.log(err);
-    //                     // res.json({msg:"password incorrect", code:401})
-    //                 }else{
-    //                     if(result.length ){
-    //                         res.json(result);
-    //                     }
-    //                     else {
-    //                         res.json({msg:"Password Incorrect", code:401})
-    //                     }
-    //                 }
-    //             }
-    //         );
-    //     }
-    //     else if (userresponse.length == 0){
-    //         res.json({msg:"Enter Credentials", code:404});
-    //     }
-    //     else {
-    //         res.json({msg:"user is not present", code:404});
-    //     }
-    // })
+    // console.log("hiiii. username", username);
+    // if (username == undefined){
+    //     console.log("nothing");
+    // }
+    // else {
+    //     console.log("something");
+    // }
+    db.query(`select username from users where username = ?`,
+    [username],
+    (err,userresponse)=>{
+        if (userresponse.length>0){
+            db.query(
+                `select username, password
+                 from users where username = ? and password = ?`,
+                [username, password],
+                (err, result)=>{
+                    if (err){
+                        console.log(err);
+                        // res.json({msg:"password incorrect", code:401})
+                    }else{
+                        if(result.length ){
+                            res.json(result);
+                        }
+                        else {
+                            res.json({msg:"Password Incorrect", code:401})
+                        }
+                    }
+                }
+            );
+        }
+        else if (userresponse.length == 0){
+            res.json({msg:"Enter Credentials", code:404});
+        }
+        else {
+            res.json({msg:"user is not present", code:404});
+        }
+    })
  
 })
 
