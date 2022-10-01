0da5f76e4527cdb341527d3dd669c73f51db52eb
Rashmi Shree S <rashmishrees@Rashmis-MacBook-Pro.local>
2022-10-01 10:28:08 +0530

    ou

--- BE/delivery.js
+++ BE/delivery.js
@@ -209,9 +209,6 @@ router.post ('/getpurchasestatusfrom', (req, res)=>{
 })
 router.put('/updateDeliveryData',(req,res)=>{
     const reqdata = req.body.params;
-    console.log("hiii rowdatadisplayed", reqdata.rowdatadisplayed);
-    console.log("hiii customerdata", reqdata.customerdata);
-    console.log("hiii customerdata", reqdata.customerdata.statename.label);
     let owner_company = reqdata.rowdatadisplayed.map((data)=>{
         return data.owner_company;
     });
@@ -221,6 +218,9 @@ router.put('/updateDeliveryData',(req,res)=>{
     let state_code = reqdata.rowdatadisplayed.map((data)=>{
         return data.state_code;
     });
+    console.log("owner_company",owner_company);
+    console.log("state",state);
+    console.log("state_code",state_code);
     var phone_number_alter_one = '';
     var phone_number_alter_two = '';
     var requested_delivery_date = '';
