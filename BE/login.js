40425658c8f6112755d27e70ff164cd352e21d54
rash010 <rashmi@sensarobotics.com>
2024-01-08 11:02:03 +0530

    check

--- BE/login.js
+++ BE/login.js
@@ -13,41 +13,41 @@ router.post('/login',(req,res)=>{
     let buffusername = new  Buffer.from(username, 'base64');
     // // The buffusername is then converted to a string in ASCII encoding, effectively decoding the base64-encoded username.
     let decodedusername = buffusername.toString('ascii');
-    console.log("buffusername,decodedusername",buffusername,decodedusername);
+    
     // // A Buffer object created from the base64-encoded password.
-    // let buffpassword = new Buffer.from(password, 'base64');
+    let buffpassword = new Buffer.from(password, 'base64');
     // // The buffpassword is then converted to a string in ASCII encoding, effectively decoding the base64-encoded password.
-    // let decodedpassword = buffpassword.toString('ascii');
-    // var error;
-    // try{
-    //     if (username.length == 0){
-    //         throw error="Username is missing";
-    //     }
-    //     else if(password.length == 0){
-    //         throw error="Password is missing";
-    //     }
-    //     else {
-    //         db.query(`select username
-    //         from users where username = ? and password = ?`,
-    //         [decodedusername, decodedpassword],
-    //         (err, result)=>{
-    //             if(result.length == 0){
-    //                 // res.status(401).send({message:"Password Incorrect"});
-    //                 res.send({message:"Password Incorrect"});
-    //             }
-    //             else{
-    //                 let string = result[0].username;
-    //                 let buffresult = Buffer.from(string, 'utf8');
-    //                 let base64String = buffresult.toString("base64");
-    //                 res.json(base64String);
-    //             }
-    //         }
-    //         )
-    //     }
-    // }
-    // catch(e){
-    //     res.send({message:error});
-    // }
+    let decodedpassword = buffpassword.toString('ascii');
+    var error;
+    try{
+        if (username.length == 0){
+            throw error="Username is missing";
+        }
+        else if(password.length == 0){
+            throw error="Password is missing";
+        }
+        else {
+            db.query(`select username
+            from users where username = ? and password = ?`,
+            [decodedusername, decodedpassword],
+            (err, result)=>{
+                if(result.length == 0){
+                    // res.status(401).send({message:"Password Incorrect"});
+                    res.send({message:"Password Incorrect"});
+                }
+                else{
+                    let string = result[0].username;
+                    let buffresult = Buffer.from(string, 'utf8');
+                    let base64String = buffresult.toString("base64");
+                    res.json(base64String);
+                }
+            }
+            )
+        }
+    }
+    catch(e){
+        res.send({message:error});
+    }
 })
 
 module.exports = router;
