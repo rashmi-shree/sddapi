678674361f6e74c2a6e4ee90a2db3df26c123470
rash010 <rashmi@sensarobotics.com>
2024-01-08 11:14:53 +0530

    db

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const db = require("./db");
 app.use(bodyParser.json());
 app.use(cors());
 
