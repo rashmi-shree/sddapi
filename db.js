f9aa5ca84624b69198b978f6b95679b713d687fa
rash010 <rashmi@sensarobotics.com>
2024-01-08 10:30:33 +0530

    extras

--- db.js
+++ db.js
@@ -1,6 +1,14 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
+    // localhost
+    // user:'root',
+    // host:'127.0.0.1',
+    // port:'3306',
+    // password : 'iwbFR$$0102',
+    // database : 'sdddb1',
+
+    // production
     user:'rashmi',
     host:'ec2-16-171-137-229.eu-north-1.compute.amazonaws.com',
     port:'3306',
