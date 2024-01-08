9214f4025f26cf6cce406b62b563eb351395bf5f
rash010 <rashmi@sensarobotics.com>
2024-01-08 13:31:10 +0530

    pool

--- db.js
+++ db.js
@@ -1,19 +1,29 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-    // localhost
-    // user:'root',
-    // host:'127.0.0.1',
-    // port:'3306',
-    // password : 'iwbFR$$0102',
-    // database : 'sdddb1',
+// const db = mysql.createConnection({
+//     // localhost
+//     // user:'root',
+//     // host:'127.0.0.1',
+//     // port:'3306',
+//     // password : 'iwbFR$$0102',
+//     // database : 'sdddb1',
 
-    // production
+//     // production
+//     user:'rashmi',
+//     host:'ec2-16-171-137-229.eu-north-1.compute.amazonaws.com',
+//     port:'3306',
+//     password : 'iwbFR$$0102',
+//     database : 'invoiceGeneratorDB'
+// });
+
+const db = mysql.createPool({
+    connectionLimit: 10, // Adjust this limit as needed
     user:'rashmi',
     host:'ec2-16-171-137-229.eu-north-1.compute.amazonaws.com',
     port:'3306',
     password : 'iwbFR$$0102',
     database : 'invoiceGeneratorDB'
-});
+  });
+  
 
 module.exports = db;
